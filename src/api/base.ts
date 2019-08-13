3443b26c0c2716759a1974c4c82459e98db51e02
normanRsharpe <sharpe.r.norman@gmail.com>
2019-08-12 21:45:04 -0600

    CORS

--- src/api/base.ts
+++ src/api/base.ts
@@ -16,7 +16,7 @@
 import { Configuration } from "./configuration";
 import globalAxios, { AxiosPromise, AxiosInstance } from 'axios';
 
-export const BASE_PATH = "http://sheltered-beach-35105.herokuapp.com/v1".replace(/\/+$/, "");
+export const BASE_PATH = "https://sheltered-beach-35105.herokuapp.com/v1".replace(/\/+$/, "");
 
 /**
  *
